e46c4fdb25a2546a54ab94fbcdcdb0770e3935bd
miltonmurray <miltonmurray@github.com>
2019-06-16 20:18:30 -0400

    Created Props

--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail />
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="Today at 5pm" 
+                content="Look up in the sky, its a bird, its a plane!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Jane" 
+                timeAgo="Today at 2am"
+                content="Nice blog post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Peter" 
+                timeAgo="Yesterday at 2:30pm"
+                content="Way to go"
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
