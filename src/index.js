b8041af33985d4aa659eb56d314efe4bd3f7dcd8
miltonmurray <miltonmurray@github.com>
2019-06-16 20:37:50 -0400

    Added resuable cards

--- src/index.js
+++ src/index.js
@@ -7,24 +7,30 @@ import ApprovalCard from './ApprovalCard';
 const App = ()=>{
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Today at 5pm" 
-                content="Look up in the sky, its a bird, its a plane!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Jane" 
-                timeAgo="Today at 2am"
-                content="Nice blog post!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-                author="Peter" 
-                timeAgo="Yesterday at 2:30pm"
-                content="Way to go"
-                avatar={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 5pm" 
+                    content="Look up in the sky, its a bird, its a plane!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    timeAgo="Today at 2am"
+                    content="Nice blog post!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Peter" 
+                    timeAgo="Yesterday at 2:30pm"
+                    content="Way to go"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
